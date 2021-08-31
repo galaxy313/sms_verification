0aea0327075b0379a869b7e80a6fcfb93238c678
ali <galaxy313@mihanmail.ir>
2021-08-31 09:19:46 -0700

    get_sms

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 app=Flask(__name__)
 
-@app.route('/')
-def main_page():
-    return 'salam'
+
+@app.route('/v1/process',methods=['POST'])
+def process():
+    data=request.form
+    sender=data["from"]
+    message=data["message"]
+    print (f'recieve {message} from {sender}')
+    ret={"message":"processed"}
+    return jsonify(ret),200
+def send_sms():
+    pass
+def check_serial():
+    pass
+if __name__ == "__main__":
+    app.run("0.0.0.0",5000,debug=True)
